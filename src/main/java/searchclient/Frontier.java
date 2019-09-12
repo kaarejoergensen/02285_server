d6e7fa512fbfff141e258d1321a0f049d1a0fe4a
Kåre Joakim Jørgensen <kaare1994@gmail.com>
2019-09-12 11:03:09 +0200

    Added goal-count heurestic

--- src/main/java/searchclient/Frontier.java
+++ src/main/java/searchclient/Frontier.java
@@ -1,30 +1,33 @@
 package searchclient;
 
-import org.w3c.dom.Node;
-
 import java.util.ArrayDeque;
 import java.util.HashSet;
 import java.util.LinkedList;
 
 public abstract class Frontier {
-    private final HashSet<State> set = new HashSet<>(65536);
+    protected final HashSet<State> set = new HashSet<>(65536);
 
     abstract void add(State s);
 
     abstract State pop();
 
-    abstract boolean isEmpty();
-
-    abstract int size();
-
-    abstract boolean contains(State s);
-
     abstract String getName();
+
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    public int size() {
+        return set.size();
+    }
+
+    public boolean contains(State s) {
+        return set.contains(s);
+    }
 }
 
 class FrontierBFS extends Frontier {
     private final ArrayDeque<State> queue = new ArrayDeque<>(65536);
-    private final HashSet<State> set = new HashSet<>(65536);
 
     @Override
     public void add(State s) {
@@ -40,21 +43,6 @@ class FrontierBFS extends Frontier {
     }
 
     @Override
-    public boolean isEmpty() {
-        return this.queue.isEmpty();
-    }
-
-    @Override
-    public int size() {
-        return this.queue.size();
-    }
-
-    @Override
-    public boolean contains(State s) {
-        return this.set.contains(s);
-    }
-
-    @Override
     public String getName() {
         return "breadth-first search";
     }
@@ -62,34 +50,18 @@ class FrontierBFS extends Frontier {
 
 class FrontierDFS extends Frontier {
     private final LinkedList<State> frontier = new LinkedList<>();
-    private final HashSet<State> frontierSet = new HashSet<>(65536);
 
     @Override
     public void add(State s) {
         frontier.add(s);
-        frontierSet.add(s);
+        set.add(s);
     }
 
     @Override
     public State pop() {
         State n = frontier.removeLast();
-        frontierSet.remove(n);
+        set.remove(n);
         return n;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public int size() {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public boolean contains(State s) {
-        throw new NotImplementedException();
     }
 
     @Override
@@ -100,6 +72,7 @@ class FrontierDFS extends Frontier {
 
 class FrontierBestFirst extends Frontier {
     private Heuristic heuristic;
+    private final ArrayDeque<State> queue = new ArrayDeque<>(65536);
 
     public FrontierBestFirst(Heuristic h) {
         this.heuristic = h;
@@ -107,27 +80,15 @@ class FrontierBestFirst extends Frontier {
 
     @Override
     public void add(State s) {
-        throw new NotImplementedException();
+        this.queue.addLast(s);
+        this.set.add(s);
     }
 
     @Override
     public State pop() {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public int size() {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public boolean contains(State s) {
-        throw new NotImplementedException();
+        State s = this.queue.pollFirst();
+        this.set.remove(s);
+        return s;
     }
 
     @Override
--- src/main/java/searchclient/Heuristic.java
+++ src/main/java/searchclient/Heuristic.java
@@ -1,6 +1,9 @@
 package searchclient;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public abstract class Heuristic implements Comparator<State> {
     public Heuristic(State initialState) {
@@ -8,7 +11,46 @@ public abstract class Heuristic implements Comparator<State> {
     }
 
     public int h(State n) {
-        throw new NotImplementedException();
+        int result = 0;
+
+        List<Location> boxes = new LinkedList<>();
+        List<Location> goals = new LinkedList<>();
+
+        for (int row = 1; row < n.boxes.length - 1; row ++) {
+            for (int col = 1; col < n.boxes[row].length - 1; col++) {
+                char g = n.goals[row][col];
+                char b = Character.toLowerCase(n.boxes[row][col]);
+                if (g > 0) {
+                    goals.add(new Location(row, col, g, Integer.MAX_VALUE));
+                }
+                if (b > 0) {
+                    boxes.add(new Location(row, col, b, Integer.MAX_VALUE));
+                }
+            }
+        }
+
+        for (Location goal : goals) {
+            Iterator<Location> itr = boxes.iterator();
+            while (itr.hasNext()) {
+                Location box = itr.next();
+                if (goal.character == box.character) {
+                    if (goal.x == box.x && goal.y == box.y) {
+                        itr.remove();
+                        break;
+                    } else {
+                        double distance = Math.sqrt(Math.pow(box.x - goal.x, 2) + Math.pow(box.y - goal.y, 2));
+                        if (distance < box.distance) {
+                            box.distance = (int) distance;
+                        }
+                    }
+                }
+            }
+        }
+        for (Location box : boxes) {
+            result += 100 * box.distance;
+            result += Math.abs((Math.sqrt(Math.pow(n.agentRows[0] - box.x, 2) + Math.pow(n.agentCols[0] - box.y, 2))) - 1);
+        }
+        return result;
     }
 
     public abstract int f(State n);
@@ -16,6 +58,20 @@ public abstract class Heuristic implements Comparator<State> {
     @Override
     public int compare(State n1, State n2) {
         return this.f(n1) - this.f(n2);
+    }
+
+    public class Location {
+        public final int x;
+        public final int y;
+        public final char character;
+        public int distance;
+
+        public Location(int x, int y, char character, int distance) {
+            this.x = x;
+            this.y = y;
+            this.character = character;
+            this.distance = distance;
+        }
     }
 }
 
@@ -69,3 +125,4 @@ class HeuristicGreedy extends Heuristic {
         return "greedy evaluation";
     }
 }
+
