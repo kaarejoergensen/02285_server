0fad89045bef514fd9b60a1473bab17556ca0927
Kåre Jørgensen <kaare1994@gmail.com>
2020-01-16 12:54:45 +0100

    Added scripts for NN benchmarking

---
+++ submit_benchmark_nn.sh
@@ -0,0 +1,34 @@
+#!/bin/sh 
+### General options
+### -- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J Benchmark
+### -- ask for number of cores (default: 1) --
+#BSUB -n 1
+### -- specify that the cores must be on the same host --
+#BSUB -R "span[hosts=1]"
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=shared:j_exclusive=yes"
+#BSUB -R "select[gpu32gb]"
+### -- specify that we need 2GB of memory per core/slot --
+#BSUB -R "rusage[mem=32GB]"
+### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot --
+#BSUB -M 32GB
+### -- set walltime limit: hh:mm --
+#BSUB -W 24:00
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u s144852@student.dtu.dk
+### -- send notification at completion --
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -oo Out_Error_Searchclient.out
+#BSUB -eo Out_Error_Searchclient.out
+
+
+# here follow the commands you want to execute 
+cd ~/Documents/02285_server
+./benchmark_NN_server.sh
