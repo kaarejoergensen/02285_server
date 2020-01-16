5f845e792b2506ea1f8370b35bdee09a7242dfb8
Kåre Jørgensen <kaare1994@gmail.com>
2020-01-16 16:34:09 +0100

    BFS memory fix

--- benchmark_BFS_server.sh
+++ benchmark_BFS_server.sh
@@ -9,14 +9,14 @@ rm -rf logs/bfs/basic/*
 rm -rf logs/bfs/randomwalk/*
 rm -rf logs/bfs/dungeon/*
 
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx32G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/basic/Easy_1 -t 1200 -o logs/bfs/basic/Easy.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx32G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/basic/Easy_2 -t 1200 -o logs/bfs/basic/Medium.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx32G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/basic/Easy_3 -t 1200 -o logs/bfs/basic/Hard.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/basic/Easy_1 -t 1200 -o logs/bfs/basic/Easy.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/basic/Easy_2 -t 1200 -o logs/bfs/basic/Medium.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/basic/Easy_3 -t 1200 -o logs/bfs/basic/Hard.zip
 
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx32G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/randomwalk/Easy_1 -t 1200 -o logs/bfs/randomwalk/Easy.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx32G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/randomwalk/Easy_2 -t 1200 -o logs/bfs/randomwalk/Medium.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx32G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/randomwalk/Easy_3 -t 1200 -o logs/bfs/randomwalk/Hard.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/randomwalk/Easy_1 -t 1200 -o logs/bfs/randomwalk/Easy.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/randomwalk/Easy_2 -t 1200 -o logs/bfs/randomwalk/Medium.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/randomwalk/Easy_3 -t 1200 -o logs/bfs/randomwalk/Hard.zip
 
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx32G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/dungeon/Easy_1 -t 1200 -o logs/bfs/dungeon/Easy.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx32G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/dungeon/Easy_2 -t 1200 -o logs/bfs/dungeon/Medium.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx32G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/dungeon/Easy_3 -t 1200 -o logs/bfs/dungeon/Hard.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/dungeon/Easy_1 -t 1200 -o logs/bfs/dungeon/Easy.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/dungeon/Easy_2 -t 1200 -o logs/bfs/dungeon/Medium.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -bfs" -l src/main/resources/levels/generated/dungeon/Easy_3 -t 1200 -o logs/bfs/dungeon/Hard.zip
