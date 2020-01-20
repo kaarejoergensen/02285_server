a6938dc108f08e2450a2906c1f95d08f7345f8b6
Kåre Jørgensen <kaare1994@gmail.com>
2020-01-20 13:13:13 +0100

    Add python path to nn benchmar

--- benchmark_NN_server.sh
+++ benchmark_NN_server.sh
@@ -9,14 +9,14 @@ rm -rf logs/nn/basic/*
 rm -rf logs/nn/randomwalk/*
 rm -rf logs/nn/dungeon/*
 
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best" -l src/main/resources/levels/generated/basic/Easy_1 -t 1200 -o logs/nn/basic/Easy.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best" -l src/main/resources/levels/generated/basic/Easy_2 -t 1200 -o logs/nn/basic/Medium.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best" -l src/main/resources/levels/generated/basic/Easy_3 -t 1200 -o logs/nn/basic/Hard.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best -python python3" -l src/main/resources/levels/generated/basic/Easy_1 -t 1200 -o logs/nn/basic/Easy.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best -python python3" -l src/main/resources/levels/generated/basic/Easy_2 -t 1200 -o logs/nn/basic/Medium.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best -python python3" -l src/main/resources/levels/generated/basic/Easy_3 -t 1200 -o logs/nn/basic/Hard.zip
 
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best" -l src/main/resources/levels/generated/randomwalk/Easy_1 -t 1200 -o logs/nn/randomwalk/Easy.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best" -l src/main/resources/levels/generated/randomwalk/Easy_2 -t 1200 -o logs/nn/randomwalk/Medium.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best" -l src/main/resources/levels/generated/randomwalk/Easy_3 -t 1200 -o logs/nn/randomwalk/Hard.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best -python python3" -l src/main/resources/levels/generated/randomwalk/Easy_1 -t 1200 -o logs/nn/randomwalk/Easy.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best -python python3" -l src/main/resources/levels/generated/randomwalk/Easy_2 -t 1200 -o logs/nn/randomwalk/Medium.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best -python python3" -l src/main/resources/levels/generated/randomwalk/Easy_3 -t 1200 -o logs/nn/randomwalk/Hard.zip
 
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best" -l src/main/resources/levels/generated/dungeon/Easy_1 -t 1200 -o logs/nn/dungeon/Easy.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best" -l src/main/resources/levels/generated/dungeon/Easy_2 -t 1200 -o logs/nn/dungeon/Medium.zip
-$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best" -l src/main/resources/levels/generated/dungeon/Easy_3 -t 1200 -o logs/nn/dungeon/Hard.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best -python python3" -l src/main/resources/levels/generated/dungeon/Easy_1 -t 1200 -o logs/nn/dungeon/Easy.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best -python python3" -l src/main/resources/levels/generated/dungeon/Easy_2 -t 1200 -o logs/nn/dungeon/Medium.zip
+$java -jar target/02285_server-1.0-SNAPSHOT-executable.jar -c "$java -Xmx70G -classpath target/classes searchclient.SearchClient -alpha -best -python python3" -l src/main/resources/levels/generated/dungeon/Easy_3 -t 1200 -o logs/nn/dungeon/Hard.zip
