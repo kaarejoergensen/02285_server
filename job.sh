435dc6df0bd1097c45492b6f69be351cdca2d7f5
Kåreååaare Joergensen <kaare1994@gmail.com>
2020-01-21 11:23:39 +0100

    Update nn training scripts

--- job.sh
+++ job.sh
@@ -1,2 +1,2 @@
 export JAVA_HOME=~/Downloads/jdk-12.0.2
-cat src/main/resources/levels/ml/01.lvl | ~/apache-maven-3.5.0/bin/mvn clean compile exec:java -Dexec.mainClass="searchclient.SearchClient" -Dexec.args="-alpha -train -python python3 -gpus 2"
+cat src/main/resources/levels/generated/basic/Easy_1/0_Basic_Easy_1.lvl | ~/apache-maven-3.5.0/bin/mvn clean compile exec:java -Dexec.mainClass="searchclient.SearchClient" -Dexec.args="-alpha -train -python python3 -gpus 2 -generate 10 basic easy_1"
